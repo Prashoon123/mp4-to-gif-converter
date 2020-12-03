e7912ec88bc76beb83d3657e5b7cd5676c0c3bf0
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-12-03 19:02:29 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,78 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
+
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState(undefined);
+  const [gif, setGif] = useState();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
   useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    load();
+  }, []);
+
+  const convert2Gif = async () => {
+    alert("Processing your request to convert this video file to a GIF.");
+    
+    // Write the file to memory
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+    // Run the FFMpeg command
+    await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', 'out.gif');
+
+    // Read the result
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    // Create a URL
+    const gifURL = URL.createObjectURL(
+      new Blob([data.buffer], { type: 'image/gif' }),
+    );
+    setGif(gifURL);
+  };
+
+  return ready ? (
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      {video && (
+        <video
+          style={{ marginTop: '20px' }}
+          controls
+          width="350"
+          src={URL.createObjectURL(video)}
+        ></video>
+      )}
+      <input
+        style={{ marginTop: '30px', marginLeft: '10px' }}
+        type="file"
+        onChange={(e) => setVideo(e.target.files?.item(0))}
+      />
+      {video && <div>
+        <h3>Result</h3>
+      <button onClick={convert2Gif}>Convert to GIF</button>
+      </div>
+      }
+      
+
+      {gif && (
+        <img style={{ marginTop: '20px' }} controls width="350" src={gif} />
+      )}
     </div>
+  ) : (
+    <p>Loading...</p>
   );
 }
 
